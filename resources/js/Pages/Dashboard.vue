1d380b209b28ab1ff1924a731b79d903a122fbde
SakCode <angelitotupapitomana@gmail.com>
2023-11-05 17:14:52 -0500

    feat:nav-aside-done

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@ import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <!---AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
@@ -18,5 +18,8 @@ import Welcome from '@/Components/Welcome.vue';
                 </div>
             </div>
         </div>
+    </AppLayout----->
+    <AppLayout>
+        Hola
     </AppLayout>
 </template>
